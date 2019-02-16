40b2e7d84aaa0aa7a83276ac8f6ab150b43dfab3
ZnarreZ <44035174+ZnarreZ@users.noreply.github.com>
2019-02-17 00:41:03 +0100

    Update makeswitches.py

--- makeswitches.py
+++ makeswitches.py
@@ -3,8 +3,8 @@ import requests
 import json
 
 #requesturl = "http://gondul.lan.sdok.no/api/write/switch-update"
-requesturl = 'http://192.168.1.206/api/write/switches'
-community = 'passord'
+requesturl = 'http://192.168.1.206/api/'
+secret = 'passord'
 switchtags = ["dlink","simplesnmp","new"]
 
 sw_x = 1080
@@ -12,8 +12,10 @@ sw_y = 290
 sw_height = 20
 sw_width = 130
 
+
 base_network_v4 = ipaddress.ip_network('213.184.214.0/23')
 subnet_size_v4 = 27
+
 base_network_v6 = ipaddress.ip_network('2a0b:5102:0:200::/59')
 subnet_size_v6 = 64
 
@@ -52,18 +54,19 @@ for row in rows:
         subnet_v4 = base_subnets_v4[net_count]
         subnet_v6 = base_subnets_v6[net_count]
         name = "E{0}-{1}".format(row_count,sw_count)
+        print(name, ' - ', placement)
         #
         gw4 = ipaddress.IPv4Network(subnet_v4)[1].exploded
         gw6 = ipaddress.IPv6Network(subnet_v6)[1].exploded
         switch4 = ipaddress.IPv4Network(subnet_v4)[2].exploded
         switch6 = ipaddress.IPv6Network(subnet_v6)[2].exploded
         #
-        data = json.dumps([{'sysname': name, 'distro_name': distro_name, 'distro_phy_port': port, 'traffic_vlan': name, 'mgmt_vlan': name, 'mgmt_v4_addr': switch4, 'mgmt_v6_addr': switch6, 'community':community,  'placement':placement, 'tags': switchtags}])
-        r = requests.post(requesturl, data=data, headers={'content-type': 'application/json'}, auth=('tech','rules'))
+        data = json.dumps([{'sysname': name, 'distro_name': distro_name, 'distro_phy_port': port, 'traffic_vlan': name, 'mgmt_vlan': name, 'mgmt_v4_addr': switch4, 'mgmt_v6_addr': switch6, 'community':secret,  'placement':placement, 'tags': switchtags}])
+        r = requests.post(requesturl + 'write/switches', data=data, headers={'content-type': 'application/json'}, auth=('tech','rules'))
         print(r.status_code, r.reason, data)
         #
         data = json.dumps([{'name': name, 'subnet4': str(subnet_v4), 'subnet6': str(subnet_v6), 'gw4': gw4, 'gw6': gw6, 'routing_point': distro_name, 'vlan': start_vlan_id, 'tags': '["dhcp", "clients"]'}])
-        r = requests.post(requesturl, data=data, headers={'content-type': 'application/json'}, auth=('tech','rules'))
+        r = requests.post(requesturl + 'write/networks', data=data, headers={'content-type': 'application/json'}, auth=('tech','rules'))
         print(r.status_code, r.reason, data)
         #
         print("{0} - {1}:{2}, {3} - {4} - {5}".format(name, distro_name, port, subnet_v4, subnet_v6, start_vlan_id))
